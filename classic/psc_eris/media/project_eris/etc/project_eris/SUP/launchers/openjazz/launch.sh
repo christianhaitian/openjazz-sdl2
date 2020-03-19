4708d4a97a84c731189334337131944b43988917
Swingflip <ryan@ryanhamlin.co.uk>
2020-03-19 17:47:29 +0000

    Fixed git ignore

---
+++ classic/psc_eris/media/project_eris/etc/project_eris/SUP/launchers/openjazz/launch.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+source "/var/volatile/project_eris.cfg"
+cd "/var/volatile/launchtmp"
+chmod +x "OpenJazz"
+echo -n 2 > "/data/power/disable"
+HOME="/var/volatile/launchtmp/gamedata" LD_PRELOAD="${PROJECT_ERIS_PATH}/lib/sdl_remap_arm.so" ./OpenJazz &> "${RUNTIME_LOG_PATH}/jazzjackrabbit.log" 
+echo -n 1 > "/data/power/disable"
+echo "launch_StockUI" > "/tmp/launchfilecommand"
